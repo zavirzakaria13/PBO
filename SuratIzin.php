60ac05d1b7301511377946952285a7ae139893b7
nadaceliasinka1212 <inesnada120@gmail.com>
2022-08-12 14:03:20 +0700

    Nada

---
+++ SuratIzin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="mb-3">
+        <label for="formFile" class="form-label">Unggah Surat Izin</label>
+        <input class="form-control" type="file" id="formFile">
+      </div>
+    
+      <div class="mb-3">
+        <label for="formFile" class="form-label">Unggah Jadwal Ujian</label>
+        <input class="form-control" type="file" id="formFile">
+      </div>
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+</body>
+</html>
