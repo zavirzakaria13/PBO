13954ce2fdae5cc575ff252bb5840947626fa280
ayaaa22 <eliyafitrinurazizah@gmail.com>
2022-08-22 10:41:33 +0700

    aya

--- pendaftaran-ujian.php
+++ pendaftaran-ujian.php
@@ -56,11 +56,210 @@
         </ul>
       </div>
       <div class="col-md-10 p-5 pt-2">
-        <h3><i class="fas fa-tachemeter-alt mr-2"></i>JADWAL UJIAN<hr>
-          
+        <h3><i class="fas fa-tachemeter-alt mr-2"></i>PENDAFTARAN UJIAN<hr>
+        <style>
+      .wrap {
+        width: 800px;
+        margin: 10px auto;
+        padding: 1px;
+      }
+    </style>
+        <div class="wrap" style="background-color: white">
+      <form>
+      <tittle>Pendaftaran Ujian Kerja Praktek</tittle>
+        <table widht="750">
+          <h3 style="background-color: grey" align="center">Informasi Personal</h3>
+          <tr>
+            <td>Nama</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>NIM</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>Tempat, tanggal lahir</td>
+            <td><input type="text" name="" /> <input type="date" name="" /></td>
+          </tr>
+          <tr>
+            <td>Alamat</td>
+            <td><textarea></textarea></td>
+          </tr>
+          <tr>
+            <td>Agama</td>
+            <td>
+              <select>
+                <option></option>
+                <option>Islam</option>
+                <option>Kristen</option>
+                <option>Hindu</option>
+                <option>Budha</option>
+                <option>Katolik</option>
+                <option>Konghucu</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Jenis Kelamin</td>
+            <td>
+              <input type="radio" name="" />Laki-Laki
 
+              <input type="radio" name="" />Perempuan
+            </td>
+          </tr>
+          <tr>
+            <td>Status</td>
+            <td>
+              <select>
+                <option></option>
+                <option>Menikah</option>
+                <option>Belum Menikah</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Kewarganegaraan</td>
+            <td>
+              <select>
+                <option></option>
+                <option>WNI</option>
+                <option>WNA</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Kelurahan</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>Kecamatan</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>Kabupaten/Kota</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>Provinsi</td>
+            <td>
+              <select>
+                <option>Pilih Provinsi</option>
+                <option value=""></option>
+                <option value="51">Aceh</option>
+                <option value="51">Sumatera Utara</option>
+                <option value="51">Sumatera Selatan</option>
+                <option value="51">Sumatera Barat</option>
+                <option value="51">Bengkulu</option>
+                <option value="51">Riau</option>
+                <option value="51">Kepulauan Riau</option>
+                <option value="51">Jambi</option>
+                <option value="51">Lampung</option>
+                <option value="51">Bangka Belitung</option>
+                <option value="51">Kalimantan Barat</option>
+                <option value="51">Kalimantan Timur</option>
+                <option value="51">Kalimantan Selatan</option>
+                <option value="51">Kalimantan Tengah</option>
+                <option value="51">Kalimantan Utara</option>
+                <option value="51">Banten</option>
+                <option value="51">DKI Jakarta</option>
+                <option value="51">Jawa Tengah</option>
+                <option value="51">Jawa Barat</option>
+                <option value="51">DI Yogyakarta</option>
+                <option value="51">Jawa Timur</option>
+                <option value="51">Bali</option>
+                <option value="51">NTT</option>
+                <option value="51">NTB</option>
+                <option value="51">Gorontalo</option>
+                <option value="51">Sulawesi Barat</option>
+                <option value="51">Sulawesi Tengah</option>
+                <option value="51">Sulawesi Utara</option>
+                <option value="51">Sulawesi Selatan</option>
+                <option value="51">Maluku Utara</option>
+                <option value="51">Maluku</option>
+                <option value="51">Papua Barat</option>
+                <option value="51">Papua (Daerah Khusus)</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>No Telpon</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+        </table>
+        <table widht="650">
+          <h3 style="background-color: grey" align="center">Informasi Universitas</h3>
+          <tr>
+            <td>Nama Universitas</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>Jurusan</td>
+            <td>
+              <select>
+                <option></option>
+                <option>Teknik Informatika</option>
+                <option>Teknik Mesin</option>
+                <option>Teknik Sipil</option>
+                <option>Teknik Manufaktur Kapal</option>
+                <option>Manajemen Bisnis Pariwisata</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Prodi</td>
+            <td>
+              <select>
+                <option></option>
+                <option>D4-Teknologi Rekayasa Perangkat Lunak</option>
+                <option>D4-Manajemen Bisnis</option>
+                <option>D4-Rekayasa Komputer</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Dosen Pembimbing</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>NIP Dosen Pembimbing</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>Tahun Lulus</td>
+            <td>
+              <select>
+                <option></option>
+                <option>2022</option>
+                <option>2023</option>
+                <option>2024</option>
+                <option>2025</option>
+                <option>2026</option>
+                <option>2027</option>
+                <option>2028</option>
+                <option>2029</option>
+                <option>2030</option>
+              </select>
+            </td>
+          </tr>
+        </table>
+        <table width="550">
+          <h3 style="background-color: grey" align="center">Informasi Perusahaan</h3>
+          <tr>
+            <td>Nama Perusahaan</td>
+            <td><input type="text" name="" /></td>
+          </tr>
+          <tr>
+            <td>Alamat</td>
+            <td><textarea></textarea></td>
+          </tr>
+        </table>
+        <button type="submit" class="btn btn-primary">Daftar</button>
+      </form>
+    </div>
       </div>
     </div>
+
+    
 
     <h1></h1>
 
