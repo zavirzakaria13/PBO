6cabd1004528c93c14702a424f1765552490e933
nadaceliasinka1212 <inesnada120@gmail.com>
2022-08-23 10:10:23 +0700

    nada

--- SuratIzin.php
+++ SuratIzin.php
@@ -4,20 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Document</title>
+    <link rel="stylesheet" href="style-si.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <div class="mb-3">
-        <label for="formFile" class="form-label">Unggah Surat Izin</label>
+<a href="Nomor.docx" download='image' class="download-btn">Download
+        <i class="fa fa-download"></i>
+    </a>
+<div class="mb-3">
+    
+        <label for="formFile" class="form-label">Upload Surat izin KP</label>
         <input class="form-control" type="file" id="formFile">
       </div>
     
-      <div class="mb-3">
-        <label for="formFile" class="form-label">Unggah Jadwal Ujian</label>
-        <input class="form-control" type="file" id="formFile">
-      </div>
-
-      <button type="submit" class="btn btn-primary">Submit</button>
+   
 </body>
 </html>
